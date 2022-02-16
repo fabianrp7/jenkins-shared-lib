37554924e3fea4a1a43600c93cae74a316e812f1
Fabian Alejandro Rodriguez Peñuela <fabian.y.rodriguez.-nd@disney.com>
2022-02-16 13:24:11 -0500

    pipeline nodejs template

---
+++ vars/pipeline_nodejs.groovy
@@ -0,0 +1,21 @@
+def call(body) {
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+    pipelineParams.each { println(it) }
+
+
+    pipeline {
+        agent { label "nodejs-agent" }
+    
+    stages {
+            stage('Build') {
+                steps {
+                    script {
+                        sh("npm install")
+                    }
+                }
+            }    
+    }
+}
