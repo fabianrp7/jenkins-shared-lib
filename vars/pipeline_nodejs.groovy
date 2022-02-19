d622a35f83de643ff5ec7118803e419e4cadab3b
Fabian Alejandro Rodriguez Peñuela <fabian.y.rodriguez.-nd@disney.com>
2022-02-19 00:48:23 -0500

    Stage: publish artifacts

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -34,8 +34,8 @@ def call(body) {
                 stage('Publish') {
                     steps {
                         script {
-                            sleep(time:60,unit:"MINUTES")
                             sh("git config --global user.email '7fabian.rodriguez@gmail.com'")
+                            sleep(time:60,unit:"MINUTES")                            
                             // sh("git checkout ${scm.branches[0].name}")
                             // sh("npm --no-git-tag-version version patch -m 'Version %s [ci-skip]'")
                             // def packageJSON = readJSON file: 'package.json'
