3381a478053998333ebe7810e6cda9c8d83496c4
Fabian Alejandro Rodriguez Peñuela <fabian.y.rodriguez.-nd@disney.com>
2022-02-19 17:10:14 -0500

    Stage: publish artifacts

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -26,66 +26,19 @@ def call(body) {
                 stage('Build') {
                     steps {
                         script {
-                            //sh("npm install") 
-                            println "test"    
-                            sleep 9000                       
+                            sh("npm install") 
                             }
                         }
                 }
 
-                // stage('Publish') {
-                //     steps {
-                //         script {
-                //             //sleep 9000
-                //             sh("npm publish") 
-                                                                                       
-                //             }
-                //         }
-                // }
+                stage('Publish') {
+                    steps {
+                        script {
+                            sh("npm publish") 
+                            }
+                        }
+                }
         }
     }
-}                
+}        
 
-// static String urlShareLibrary = "https://github.com/fabianrp7/jsl.git"
-//    static String gitCredentialsId = "github_login"
-//    static String nameJenkinsSharedLib = "jsl"
-//    static String nexusCredentialsId = "nexus-login" 
-//    static String nexusUrl = "34.123.87.228:30777"   
-//    static String nexusGroupId = "raw"
-//    static String nexusVersion = "nexus3"  
-//    static String nexusRepository = 'artifacts_'
-
-
-//         void nexusArtifactUpload(project){
-//         def now = new Date()
-//         def creationDate = now.format("yyyyMMddHHmm", TimeZone.getTimeZone("GMT-5:00"))
-//             nexusArtifactUploader(
-//                     nexusVersion: 'nexus3',
-//                     protocol: 'http',
-//                     nexusUrl: 'http://34.122.90.105:30707/',
-//                     groupId: '',
-//                     version: scriptPL.env.JOB_BASE_NAME+"_${scriptPL.env.BUILD_ID}_${creationDate}",                    
-//                     repository: GlobalVars.nexusRepository+scriptPL.env.JOB_BASE_NAME,
-//                     credentialsId: 'nexus-login',
-//                     artifacts: [
-//                         [artifactId: project.name,
-//                         classifier: '',
-//                         file: project.artifactFile,
-//                         type: 'zip']
-//                     ]
-//                     )
-//     }
-
-
-
-
-
-//     }
-
-                   
-
-                
-                
-//         }
-//     }
-// }
