fdd97cd60dc7d4af8bfec5b3c97600a3f3b59a64
Fabian Alejandro Rodriguez Peñuela <fabian.y.rodriguez.-nd@disney.com>
2022-02-20 00:13:49 -0500

    Stage: Docker build image  and push

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -16,37 +16,37 @@ def call(body) {
                     }
                 }
 
-                // stage('Build') {
-                //     steps {
-                //         script {
-                //             sh("npm install") 
-                //             }
-                //         }
-                // }
-
-                // stage('Publish') {
-                //     steps {
-                //         script {
-                //             sh("npm publish") 
-                //             }
-                //         }
-                // }
-
-                stage('Docker build & push image') {
+                stage('Build') {
                     steps {
                         script {
-                            def packageJSON = readJSON file: 'package.json'
-                            sleep 9000
-                            sh("mv Dockerfile Dockerfile-${packageJSON.version}")
-                            sh("kubectl cp Dockerfile-${packageJSON.version} dood:/home -n gorilla-logic") 
-                            sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${packageJSON.version} /home/ --tag fabianrp7/timeoff:${packageJSON.version}") 
-                            sh("kubectl exec dood -- docker login --username=fabianrp7 --password=fabrodpe2077*") 
-                            sh("kubectl exec dood -- docker push fabianrp7/timeoff:${packageJSON.version}")
-                            sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${packageJSON.version}")
-                            sh("kubectl exec dood -- rm Dockerfile-${packageJSON.version}")
+                            sh("npm install") 
                             }
                         }
                 }
+
+                stage('Publish') {
+                    steps {
+                        script {
+                            sh("npm publish") 
+                            }
+                        }
+                }
+
+                // stage('Docker build & push image') {
+                //     steps {
+                //         script {
+                //             def packageJSON = readJSON file: 'package.json'
+                //             sleep 9000
+                //             sh("mv Dockerfile Dockerfile-${packageJSON.version}")
+                //             sh("kubectl cp Dockerfile-${packageJSON.version} dood:/home -n gorilla-logic") 
+                //             sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${packageJSON.version} /home/ --tag fabianrp7/timeoff:${packageJSON.version}") 
+                //             sh("kubectl exec dood -- docker login --username=fabianrp7 --password=fabrodpe2077*") 
+                //             sh("kubectl exec dood -- docker push fabianrp7/timeoff:${packageJSON.version}")
+                //             sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${packageJSON.version}")
+                //             sh("kubectl exec dood -- rm Dockerfile-${packageJSON.version}")
+                //             }
+                //         }
+                // }
         }
     }
 }
