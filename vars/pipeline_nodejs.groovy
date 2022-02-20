2535d28af563ff5891d4d20d3480b0bd929ff8f7
Fabian Alejandro Rodriguez Peñuela <fabian.y.rodriguez.-nd@disney.com>
2022-02-20 10:44:03 -0500

    Kubernetes Deploy

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -24,17 +24,17 @@ def call(body) {
 
                 stage('Docker build & push image') {
                             pkgVersion = readJSON file: 'package.json'
-                            sh("mv Dockerfile Dockerfile-${pkgVersion}")
-                            sh("kubectl cp Dockerfile-${pkgVersion} dood:/home -n gorilla-logic") 
-                            sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${pkgVersion} /home/ --tag fabianrp7/timeoff:${pkgVersion}") 
+                            sh("mv Dockerfile Dockerfile-${pkgVersion.version}")
+                            sh("kubectl cp Dockerfile-${pkgVersion.version} dood:/home -n gorilla-logic") 
+                            sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${pkgVersion.version} /home/ --tag fabianrp7/timeoff:${pkgVersion.version}") 
                             sh("kubectl exec dood -- docker login --username=fabianrp7 --password=fabrodpe2077*") 
-                            sh("kubectl exec dood -- docker push fabianrp7/timeoff:${pkgVersion}")
-                            sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${pkgVersion}")
-                            sh("kubectl exec dood -- rm /home/Dockerfile-${pkgVersion}")                          
+                            sh("kubectl exec dood -- docker push fabianrp7/timeoff:${pkgVersion.version}")
+                            sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${pkgVersion.version}")
+                            sh("kubectl exec dood -- rm /home/Dockerfile-${pkgVersion.version}")                          
                 }
 
                 stage('Kubernetes Deploy') {
-                            sh("sed -i 's/timeoff:tag/timeoff:${pkgVersion}/g' deployment.yaml")
+                            sh("sed -i 's/timeoff:tag/timeoff:${pkgVersion.version}/g' deployment.yaml")
                             sh("kubectl apply -f deployment.yaml -n gorilla-logic")                                                     
                 }
     }
