43b7a7e2f40174ea649e3d76f210e0f1e40bb86d
Fabian Alejandro Rodriguez Peñuela <fabian.y.rodriguez.-nd@disney.com>
2022-02-20 10:40:28 -0500

    Kubernetes Deploy

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -1,5 +1,6 @@
 def call(body) {    
         node{ 
+            def pkgVersion = ""
                 stage('Checkout') {
                     if (binding.hasVariable('post_0_message')) {
                         if (post_0_message.contains('ci-skip')){
@@ -22,18 +23,18 @@ def call(body) {
                 }
 
                 stage('Docker build & push image') {
-                            def packageJSON = readJSON file: 'package.json'
-                            sh("mv Dockerfile Dockerfile-${packageJSON.version}")
-                            sh("kubectl cp Dockerfile-${packageJSON.version} dood:/home -n gorilla-logic") 
-                            sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${packageJSON.version} /home/ --tag fabianrp7/timeoff:${packageJSON.version}") 
+                            pkgVersion = readJSON file: 'package.json'
+                            sh("mv Dockerfile Dockerfile-${pkgVersion}")
+                            sh("kubectl cp Dockerfile-${pkgVersion} dood:/home -n gorilla-logic") 
+                            sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${pkgVersion} /home/ --tag fabianrp7/timeoff:${pkgVersion}") 
                             sh("kubectl exec dood -- docker login --username=fabianrp7 --password=fabrodpe2077*") 
-                            sh("kubectl exec dood -- docker push fabianrp7/timeoff:${packageJSON.version}")
-                            sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${packageJSON.version}")
-                            sh("kubectl exec dood -- rm /home/Dockerfile-${packageJSON.version}")                          
+                            sh("kubectl exec dood -- docker push fabianrp7/timeoff:${pkgVersion}")
+                            sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${pkgVersion}")
+                            sh("kubectl exec dood -- rm /home/Dockerfile-${pkgVersion}")                          
                 }
 
                 stage('Kubernetes Deploy') {
-                            sh("sed -i 's/timeoff:tag/timeoff:${packageJSON.version}/g' deployment.yaml")
+                            sh("sed -i 's/timeoff:tag/timeoff:${pkgVersion}/g' deployment.yaml")
                             sh("kubectl apply -f deployment.yaml -n gorilla-logic")                                                     
                 }
     }
