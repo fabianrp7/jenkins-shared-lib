c68188326f1af6bc7e3a109c04789d5b228273d2
Fabian Alejandro Rodriguez Peñuela <fabian.y.rodriguez.-nd@disney.com>
2022-02-20 08:42:14 -0500

    Stage: Docker build image  and push

--- vars/pipeline_nodejs.groovy
+++ vars/pipeline_nodejs.groovy
@@ -1,13 +1,14 @@
 def call(body) {    
         node{ 
                 stage('Checkout') {
-                    // if (binding.hasVariable('post_0_message')) {
-                    //     if (post_0_message.contains('ci-skip')){
-                    //         println post_0_message
-                    //         currentBuild.result = 'ABORTED'
-                    //         error("ci-skip in last commit")                                 
-                    //     }
-                    // }
+                    if (binding.hasVariable('post_0_message')) {
+                        if (post_0_message.contains('ci-skip')){
+                            println post_0_message
+                            currentBuild.result = 'ABORTED'
+                            error("ci-skip in last commit")                                 
+                        }
+                    }
+
                     cleanWs()
                     checkout scm
                 }
@@ -20,22 +21,19 @@ def call(body) {
 
                 stage('Publish') {
 
-                            sh("npm publish") 
+                    sh("npm publish") 
 
                 }
 
-                // stage('Docker build & push image') {
-
-                //             def packageJSON = readJSON file: 'package.json'
-                //             sleep 9000
-                //             sh("mv Dockerfile Dockerfile-${packageJSON.version}")
-                //             sh("kubectl cp Dockerfile-${packageJSON.version} dood:/home -n gorilla-logic") 
-                //             sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${packageJSON.version} /home/ --tag fabianrp7/timeoff:${packageJSON.version}") 
-                //             sh("kubectl exec dood -- docker login --username=fabianrp7 --password=fabrodpe2077*") 
-                //             sh("kubectl exec dood -- docker push fabianrp7/timeoff:${packageJSON.version}")
-                //             sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${packageJSON.version}")
-                //             sh("kubectl exec dood -- rm Dockerfile-${packageJSON.version}")
-                           
-                // }
+                stage('Docker build & push image') {
+                            def packageJSON = readJSON file: 'package.json'
+                            sh("mv Dockerfile Dockerfile-${packageJSON.version}")
+                            sh("kubectl cp Dockerfile-${packageJSON.version} dood:/home -n gorilla-logic") 
+                            sh("kubectl exec dood -- docker image build -f /home/Dockerfile-${packageJSON.version} /home/ --tag fabianrp7/timeoff:${packageJSON.version}") 
+                            sh("kubectl exec dood -- docker login --username=fabianrp7 --password=fabrodpe2077*") 
+                            sh("kubectl exec dood -- docker push fabianrp7/timeoff:${packageJSON.version}")
+                            sh("kubectl exec dood -- docker rmi fabianrp7/timeoff:${packageJSON.version}")
+                            sh("kubectl exec dood -- rm Dockerfile-${packageJSON.version}")                           
+                }
     }
 }
